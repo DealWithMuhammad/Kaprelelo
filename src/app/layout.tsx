a632e1939cb8c1c9cdea710c9e36e16c8afc863e
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2024-05-30 16:55:21 +0800

    feat: add createContext and GlobalState

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
+import GlobalState from "@/context/index";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <GlobalState>
+          <Navbar />
+          <main>{children}</main>
+        </GlobalState>
+      </body>
     </html>
   );
 }
